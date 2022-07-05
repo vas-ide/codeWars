e027e60427810e72d4a8e6fa177496c28010a42d
vas-ide <vas.ide2022@gmail.com>
2022-07-05 16:34:54 +0300

    Init: Contact       dell dub (Set)       Sort()

---
+++ Merge two sorted arrays into one.js
@@ -0,0 +1,25 @@
+
+function mergeArrays(arr1, arr2) {
+    let all = [arr1 + arr2]
+    const allContact = [].concat(arr1, arr2)               //Объединение
+    const allSet = Array.from(new Set(allContact))         //Удаление дублей
+    const allSort = allSet.sort(compareNumeric)            //Сортировка Sort() используя одну из функций
+    //console.log(allContact)
+    console.log(allSort)
+
+    function compareNumeric(a, b) {
+        if (a > b) return 1;
+        if (a === b) return 0;
+        if (a < b) return -1;
+    }
+
+    function compareNumbers(a, b) {
+        return a - b;
+    }
+
+}
+
+
+mergeArrays([1,2,3,4], [5,6,7,8])
+mergeArrays([1,3,5,7,9], [10,8,6,4,2])
+mergeArrays([1,3,5,7,9,11,12], [1,2,3,4,5,10,12])
